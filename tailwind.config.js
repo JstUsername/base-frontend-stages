a804a4803cb0bf576977e1ef2204d65d4ecf912b
JstUsername <g4ndy_c4ndy@mail.ru>
2024-04-28 05:50:52 +0300

    Updated tailwind.config.css
    Added fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/*.html'],
   theme: {
-    extend: {},
+    fontFamily: {
+      roboto_regular : ["roboto_regular", "sans-serif"],
+      roboto_medium : ["roboto_medium", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        'blue' : '#5095EC',
+        'black' : '#505050',
+        'second_tx' : '#8F8F8F',
+        'grey' : '#D3D8DE',
+        'bg' : '#F1F1F1',
+        'bg_2' : '#FAFAFA',
+      },
+    },
   },
   plugins: [],
 }
