6722049fc65bf653987bc939ecf9286b6cd0b69d
JstUsername <g4ndy_c4ndy@mail.ru>
2024-04-28 14:42:40 +0300

    Updated webpack.config.js
    Added mode configuration

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 const config = {
+    mode: "development",
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
